7fc7e3988a0ed8496b284c1812c78d695c8aaca5
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-25 14:41:54 +0000

    Adding private method in comments, not yet working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,22 +8,23 @@ class DockingStation
 
   attr_reader :bikes
 
-  def empty?
-    true if @bikes.size == 0
-  end
-
   def release_bike
     raise "No bikes at the station" if empty?
     @bikes.pop
   end
 
-  def full?
-    true if @bikes.size >= 20
-  end
-
   def dock(bike)
     raise "Docking station is full" if full?
     @bikes << bike
+  end
+
+# private
+  def empty?
+    true if @bikes.size == 0
+  end
+
+  def full?
+    true if @bikes.size >= 20
   end
 
 end
